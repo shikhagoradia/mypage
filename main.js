36abd1cda12f4adecb7f28aa806c0bba6ba62e34
shikhagoradia <86233008+shikhagoradia@users.noreply.github.com>
2021-06-21 15:45:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function add() {
+    var result =
+        Number(document.getElementById("firstno").value) +
+        Number(document.getElementById("secondno").value);
+    window.alert(result);
+}
